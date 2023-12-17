cc4af4d30715eebac74f1953a20fded003356e55
Filip Stamenkovic <fstamenkovic26@gmail.com>
2023-12-17 13:08:02 +0100

    Kreirani User resource, controller i resource route

---
+++ pubkvizbackend/app/Http/Resources/UserResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return[
+            'id' =>  $this->resource->id,
+            'username' => $this->resource->username,
+            'email' => $this->resource->email,
+            'full_name' => $this->resource->full_name,
+            'role' => $this->resource->role,
+            'team' =>  $this->resource->team
+        ];
+    }
+}
